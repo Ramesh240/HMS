387f410fec24f15911570c665daef18a4eb7bf4f
barnalaganesh95 <barnalaganesh13@gmail.com>
2022-06-07 17:50:58 +0530

    email added

--- src/PasswordPage.js
+++ src/PasswordPage.js
@@ -36,6 +36,8 @@ alert("you have created a new Password");
             <div className='row h-100 justify-content-center align-items-center'>
             
           <form onSubmit={this.handleSubmit} className="form-example">
+            Email
+            <input type="email" name="email" onChange={this.handleChange} className="form-control email" required/>
           Create Password 
           <input type="password" name="Password"  onChange={this.handleChange} className="form-control password" required/>
         <br></br>
